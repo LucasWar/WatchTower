2bab523ad063c0f81f119ae84fa5e493051ada05
LucasWar <lucas.warleyrv@gmail.com>
2026-06-12 13:08:08 -0300

    feat: Implementation of queuing systems with BullMq + Redis

---
+++ api/prisma/migrations/20260612140153_default_value_created_at/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "logs" ALTER COLUMN "created_at" DROP NOT NULL,
+ALTER COLUMN "created_at" SET DEFAULT CURRENT_TIMESTAMP;
--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LogsModule } from './modules/logs/logs.module';
+import { DatabaseModule } from './database/database.module';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
-  imports: [],
+  imports: [
+    LogsModule,
+    DatabaseModule,
+    BullModule.forRoot({
+      connection: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
